0942dd5b8de978cdac291eadbfef8d15582137ad
FRC9149 <FRC9149@rfsd.k12.wi.us>
2024-09-29 18:30:12 -0500

    test

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import { create3dObject } from "https://raw.githubusercontent.com/FRC9149/3d_JS_Library/refs/heads/main/three.js?token=GHSAT0AAAAAACYFBPVWQ7HTCLM3J6QSNL4YZXZ3SMA"
+import { test } from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/two.js"
 
-create3dObject(
-  document.getElementById("Jack"),
-  200,
-  200,
-  true,
-  "./models/piplup.obj"
-);
+//create3dObject(
+//  document.getElementById("Jack"),
+//  200,
+//  200,
+//  true,
+//  "./models/piplup.obj"
+//);
+
+test();
