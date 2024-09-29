26675a40423ee01b09c379a3b383e9e00e5e9f59
FRC9149 <158180802+FRC9149@users.noreply.github.com>
2024-09-29 18:02:18 -0500

    updated example to pull from github

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { create3dObject } from "./three.js"
+import { create3dObject } from "https://raw.githubusercontent.com/FRC9149/3d_JS_Library/refs/heads/main/three.js?token=GHSAT0AAAAAACYFBPVWQ7HTCLM3J6QSNL4YZXZ3SMA"
 
 create3dObject(
   document.getElementById("Jack"),
