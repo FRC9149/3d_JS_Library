8ed2bc9784295c067759552e6157713abfa79e80
FRC9149 <FRC9149@rfsd.k12.wi.us>
2024-09-29 20:25:46 -0500

    revert with rename

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-import * as ASD from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/asdf.js";
+import { create3dObject } from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/asdf.js";
 
-//create3dObject(
-//  document.getElementById("Jack"),
-//  200,
-//  200,
-//  true,
-//  "./models/piplup.obj"
-//);
-
-console.log("ASD");
+create3dObject(
+  document.getElementById("Jack"),
+  200,
+  200,
+  true,
+  "./models/piplup.obj"
+);
