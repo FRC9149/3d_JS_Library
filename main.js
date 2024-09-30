d8be8e72e1cf638cbe9220babdbae372ecb15381
FRC9149 <FRC9149@rfsd.k12.wi.us>
2024-09-29 20:27:27 -0500

    rename to update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { create3dObject } from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/asdf.js";
+import { create3dObject } from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/3d.js";
 
 create3dObject(
   document.getElementById("Jack"),
