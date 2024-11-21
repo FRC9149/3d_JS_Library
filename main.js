ea5aad07418eb11e48ea5abf9dbc1a53b326a0bc
El <105391840+El-404@users.noreply.github.com>
2024-11-21 16:34:27 -0600

    typo in main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as Renderer from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@example/9149_Rendering2.js";
 
 Renderer.create3dObject(
-  document.getElementById("contained3d"),
+  document.getElementById("container3d"),
   "/models/cali",
   400,
   500,
